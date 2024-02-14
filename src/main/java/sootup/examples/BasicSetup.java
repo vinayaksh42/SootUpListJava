fc70cae3934d5b304f1d7e511f4bff67b1b2da31
Vinayak Sharma <v236shar1@uwaterloo.ca>
2024-02-14 11:58:25 -0500

    code cleanup

--- src/main/java/sootup/examples/BasicSetup.java
+++ src/main/java/sootup/examples/BasicSetup.java
@@ -3,45 +3,25 @@ package sootup.examples;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import com.googlecode.dex2jar.ir.expr.NewExpr;
-import com.ibm.wala.dalvik.dex.instructions.NewArray;
 
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.jimple.basic.Value;
 import sootup.core.jimple.common.stmt.Stmt;
-import sootup.core.model.Position;
 import sootup.core.model.SootClass;
 import sootup.core.model.SootMethod;
 import sootup.core.model.SourceType;
 import sootup.core.signatures.MethodSignature;
-import sootup.core.types.ArrayType;
 import sootup.core.types.ClassType;
 import sootup.core.types.ReferenceType;
 import sootup.core.views.View;
 import sootup.java.bytecode.inputlocation.PathBasedAnalysisInputLocation;
 import sootup.java.core.views.JavaView;
 import sootup.core.jimple.common.stmt.AbstractDefinitionStmt;
-import sootup.core.jimple.common.stmt.AbstractStmt;
-import sootup.core.jimple.basic.Immediate;
-import sootup.core.jimple.basic.StmtPositionInfo;
 import sootup.core.jimple.common.expr.AbstractInvokeExpr;
-import sootup.core.jimple.common.expr.JNewArrayExpr;
 import sootup.core.jimple.common.expr.JNewExpr;
-import sootup.core.jimple.common.ref.JArrayRef;
-import sootup.core.jimple.common.stmt.JIfStmt;
-import sootup.core.jimple.basic.JimpleComparator;
-import sootup.core.jimple.basic.LValue;
 import sootup.core.jimple.common.stmt.JAssignStmt;
 import sootup.core.jimple.common.stmt.JGotoStmt;
-import sootup.core.types.ReferenceType;
-import sootup.core.types.Type;
-import sootup.core.types.ClassType;
-import sootup.core.signatures.MethodSignature;
-import sootup.core.jimple.common.constant.MethodType;
 
 public class BasicSetup {
 
@@ -84,16 +64,21 @@ public class BasicSetup {
     boolean isArraySafe = false;
     int arrayUnsafeUsage = 0;
 
+    MethodSignature arrayListIterator = view.getIdentifierFactory().getMethodSignature("java.util.List",
+        "iterator",
+        "java.util.Iterator", Collections.emptyList());
+    MethodSignature arrayListGet = view.getIdentifierFactory().getMethodSignature("java.util.List", "get",
+        "java.lang.Object", Collections.singletonList("int"));
+    MethodSignature arrayListIsEmpty = view.getIdentifierFactory().getMethodSignature("java.util.List", "isEmpty",
+        "boolean", Collections.emptyList());
+    ReferenceType arrayListType = (ReferenceType) view.getIdentifierFactory()
+        .getClassType("java.util.ArrayList");
+
     // iterate over the sootMethod Jimple statements
     for (Stmt stmt : sootMethod.getBody().getStmts()) {
-      System.out.println(stmt);
-
       if (stmt.containsInvokeExpr()) {
         AbstractInvokeExpr invokeExpr = (AbstractInvokeExpr) stmt.getInvokeExpr();
-
         // check if the invoke expression is a call to the get method of an ArrayList
-        MethodSignature arrayListGet = view.getIdentifierFactory().getMethodSignature("java.util.List", "get",
-            "java.lang.Object", Collections.singletonList("int"));
         if (invokeExpr.getMethodSignature().equals(arrayListGet)) {
           List<Value> InvokeExprUses = invokeExpr.getUses();
           // iterate over the uses and check if it is part of the list of variables
@@ -110,8 +95,6 @@ public class BasicSetup {
 
         // check if the invoke expression is a call to the isEmpty method of an
         // ArrayList
-        MethodSignature arrayListIsEmpty = view.getIdentifierFactory().getMethodSignature("java.util.List", "isEmpty",
-            "boolean", Collections.emptyList());
         if (invokeExpr.getMethodSignature().equals(arrayListIsEmpty)) {
           List<Value> InvokeExprUses = invokeExpr.getUses();
           for (Value use : InvokeExprUses) {
@@ -123,9 +106,6 @@ public class BasicSetup {
 
         // check if the invoke expression is a call to the iterator method of an
         // ArrayList
-        MethodSignature arrayListIterator = view.getIdentifierFactory().getMethodSignature("java.util.List",
-            "iterator",
-            "java.util.Iterator", Collections.emptyList());
         if (invokeExpr.getMethodSignature().equals(arrayListIterator)) {
           List<Value> InvokeExprUses = invokeExpr.getUses();
           // iterate over the uses and check if it is part of the list of variables
@@ -140,10 +120,9 @@ public class BasicSetup {
           }
         }
       } else if (stmt instanceof JGotoStmt) {
-        System.out.println("This is a goto statement");
+        continue;
       } else if (stmt.branches()) {
-        JIfStmt ifstmt = (JIfStmt) stmt;
-        System.out.println("Condition: " + ifstmt.getCondition());
+        continue;
       } else if (stmt instanceof JAssignStmt) {
         AbstractDefinitionStmt defstmt = (AbstractDefinitionStmt) stmt;
 
@@ -157,8 +136,6 @@ public class BasicSetup {
         // check if the right operand is a new array expression
         if (defstmt.getRightOp() instanceof JNewExpr) {
           JNewExpr newExpr = (JNewExpr) defstmt.getRightOp();
-          ReferenceType arrayListType = (ReferenceType) view.getIdentifierFactory()
-              .getClassType("java.util.ArrayList");
           if (newExpr.getType().equals(arrayListType)) {
             TempNames.add(defstmt.getLeftOp());
           }
